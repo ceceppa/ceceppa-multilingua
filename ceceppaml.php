35319fe3cb0e7359e9ddc4d609687c2cf57ec794
ceceppa <info@alessandrosenese.eu>
2014-10-15 23:42:03 +0100

    test trunk

--- ceceppaml.php
+++ ceceppaml.php
@@ -3,7 +3,7 @@
 Plugin Name: Ceceppa Multilingua
 Plugin URI: http://www.ceceppa.eu/portfolio/ceceppa-multilingua/
 Description: Adds userfriendly multilingual content management and translation support into WordPress.
-Version: 1.4.99
+Version: 1.4.999
 Author: Alessandro Senese aka Ceceppa
 Author URI: http://www.alessandrosenese.eu/
 License: GPL3
